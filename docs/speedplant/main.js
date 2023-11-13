b90765e4f657b362a2b1008737cf240cf10e6b86
Michael <xxtwomadxx@gmail.com>
2023-11-12 22:33:44 -0800

    saving changes. implemented growing plant

--- docs/speedplant/main.js
+++ docs/speedplant/main.js
@@ -1,6 +1,8 @@
 /**
  * @typedef {{
  * pos: Vector,
+ * item: string,
+ * speed: number,
  * }} Player
  */
 
@@ -13,6 +15,25 @@ let player;
  * @typedef {{
  * pos: Vector,
  * speed: number,
+ * }} Seed
+ */
+
+/**
+ * @type {Seed []}
+ */
+let seeds;
+
+/**
+ * @type { number }
+ */
+let seedSpawnCooldown;
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * height: number,
+ * speed: number,
+ * limit: number,
  * }} Plant
  */
 
@@ -21,21 +42,19 @@ let player;
  */
 let plants;
 
-/**
- * @type { number }
- */
-let plantSpawnCooldown;
-
 const G = {
-	WIDTH: 480,
+	WIDTH: 100,
 	HEIGHT: 75,
 
-	PLAYER_RUN_SPEED: 5,
+	PLAYER_RUN_SPEED: 1,
 
 	PLANT_GROW_RATE: 1,
-	PLANT_SPAWN_RATE: 90,
-	PLANT_SPEED_MIN: 0.5,
-	PLANT_SPEED_MAX: 1.0,
+
+	SEED_SPAWN_RATE: 90,
+	SEED_SPEED_MIN: 0.5,
+	SEED_SPEED_MAX: 1.0,
+
+	DOOM_TIMER: 100,
 	
 	// RAIN_DROP_RATE: 1,
 	// RAIN_FALL_SPEED: 1,
@@ -47,10 +66,26 @@ const G = {
 title = "SPEED PLANT";
 
 description = `
-Grow as much as you can!
+[Tap] pickup
 `;
 
-characters = [];
+characters = [
+`
+  rr  
+rrrrrr
+  rr  
+  rr
+rrrrrr
+r    r
+`,
+`
+  p  
+ ppp 
+ppppp
+ppppp
+ ppp 
+`,
+];
 
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT},
@@ -59,57 +94,102 @@ options = {
 function update() {
 	if (!ticks) {		
 		// initial plant spawn
-		plants = times(3, ()=>{
+		seeds = times(3, ()=>{
 			const posX = rnd(0, G.WIDTH);
 			return{
 				pos:vec(posX,0),
-				speed:rnd(G.PLANT_SPEED_MIN,G.PLANT_SPEED_MAX)
+				speed:rnd(G.SEED_SPEED_MIN,G.SEED_SPEED_MAX)
 			};
 		});
 
-		plantSpawnCooldown = G.PLANT_SPAWN_RATE;
+		seedSpawnCooldown = G.SEED_SPAWN_RATE;
 
 		player = {
-			pos: vec(G.WIDTH*0.5,G.HEIGHT*0.9),
+			pos: vec(G.WIDTH*0.5,G.HEIGHT*0.86),
+			item: "",
+			speed: G.PLAYER_RUN_SPEED,
 		}
+
+		plants = [];
 	}
 
 	// draw the player
 	color("cyan");
-	box(player.pos, 4);
+	// box(player.pos, 4);
+	char("a", player.pos);
+	if(player.item == "seed"){
+		color("purple")
+		char("b", player.pos.x, player.pos.y - 6);
+	}
 
 	// player constant movement
-	player.pos.x += G.PLAYER_RUN_SPEED;
+	player.pos.x += player.speed;
 
 	// player position wraparound
 	player.pos.wrap(0,G.WIDTH,0,G.HEIGHT);
 
 	// ground
 	color("green");
-	box(vec(G.WIDTH*0.5,G.HEIGHT*0.97), G.WIDTH, G.HEIGHT * 0.1);
+	box(vec(G.WIDTH*0.5,G.HEIGHT*0.97), G.WIDTH, G.HEIGHT * 0.15);
 
 	// spawn timer
-	plantSpawnCooldown--;
-	if(plantSpawnCooldown <= 0){
-		plants.push({
+	seedSpawnCooldown--;
+	if(seedSpawnCooldown <= 0){
+		seeds.push({
 			pos:vec(rnd(0, G.WIDTH), 0),
-			speed:rnd(G.PLANT_SPEED_MIN, G.PLANT_SPEED_MAX)
+			speed:rnd(G.SEED_SPEED_MIN, G.SEED_SPEED_MAX)
 		});
-		plantSpawnCooldown = G.PLANT_SPAWN_RATE;
+		seedSpawnCooldown = G.SEED_SPAWN_RATE;
 	}
 
 	// individual plant behavior
-	plants.forEach((p)=>{
+	seeds.forEach((seed)=>{
 		
 		// floor check
-		const isCollidingWithFloor = box(p.pos, 4).isColliding.rect.green;
-		if(!isCollidingWithFloor){
-			text("YIPEE", 3, 10);
-			p.pos.y += p.speed;
+		const isCollidingWithFloor = char("b",seed.pos).isColliding.rect.green;
+		if(seed.pos.y <= 64){
+			seed.pos.y += seed.speed;
 		}
 
 		color("purple");
-		box(p.pos,4);
+		char("b", seed.pos);
 	});
 
+	// handle seed removal
+	remove(seeds, (plant)=>{
+		const isCollidingWithPlayer = char("b",plant.pos).isColliding.char.a;
+		if(isCollidingWithPlayer && input.isJustPressed && player.item != "seed"){
+			play("select");
+		}
+		return((isCollidingWithPlayer && input.isJustPressed && (player.item != "seed")));
+	})
+
+	// plant player interaction check
+	color("cyan")
+	const isCollidingWithPlant = char("a", player.pos).isColliding.char.b;
+	if(input.isJustPressed){
+		if(isCollidingWithPlant){
+			player.item = "seed";
+		}
+		else if(player.item == "seed"){
+			// PLANT SEED IN EMPTY SPOT
+			plants.push({
+				pos: vec(player.pos.x, 69),
+				height: 0,
+				speed: G.PLANT_GROW_RATE + (1/difficulty),
+				limit: 30,
+			})
+			player.item = "";
+			play("select", {
+				pitch: 50,
+			});
+		}
+	}
+
+	plants.forEach((plant)=>{
+		// plant.height += plant.speed;
+		plant.height += 0.1;
+		color("light_red");
+		box(plant.pos.x, plant.pos.y - plant.height/2, 5, plant.height);
+	})
 }
